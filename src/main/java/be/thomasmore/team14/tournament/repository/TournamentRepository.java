af7dfe782f1d39e36a575bee8a1d7fc66e865812
AliKiyak <r0702374@student.thomasmore.be>
2019-11-10 11:59:18 +0100

    filter en delete in de repository toegevoegd

--- src/main/java/be/thomasmore/team14/tournament/repository/TournamentRepository.java
+++ src/main/java/be/thomasmore/team14/tournament/repository/TournamentRepository.java
@@ -9,4 +9,7 @@ import java.util.List;
 public interface TournamentRepository extends MongoRepository<Tournament, String> {
     List<Tournament> findTournamentByGameId(@Param("gameid") String gameId);
     Tournament findTournamentById(@Param("tournamentid") String tournamentId);
+    List<Tournament> findTournamentByNameContainingIgnoreCase(@Param("name") String name);
+    long deleteTournamentsByGameId(@Param("gameid") String gameId);
+
 }
